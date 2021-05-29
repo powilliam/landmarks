a0769019794ebc99964836db3890e8266d9d7585
William Porto <william.porto@nave.rs>
2021-05-29 20:15:42 -0300

    filtering landmarks by favorite

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -8,11 +8,27 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var onlyFavorites: Bool = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!onlyFavorites || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            List(landmarks) { landmark in
-                NavigationLink(destination: LandmarkDetails(landmark: landmark)) {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $onlyFavorites) {
+                    Text("Favorites")
+                        .font(.body)
+                        .foregroundColor(.secondary)
+                }
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink(destination: LandmarkDetails(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
             .navigationTitle("Landmarks")
